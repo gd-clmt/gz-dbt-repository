dc0e9be5337705fdf43c88b8f9aeb35feee4ce7f
Clément G. <gandouinclement@gmail.com>
2025-07-01 13:08:00 +0000

    Création de la vue "margin"

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,20 @@
+WITH sub_join_products_sales AS (
+
+SELECT 
+sales.date_date
+, sales.orders_id
+, sales.products_id
+, sales.revenue
+, sales.quantity
+, product.purchase_price
+, ROUND((sales.quantity * product.purchase_price),2) AS purchase_cost
+FROM {{ ref('stg_raw__sales') }} AS sales
+LEFT JOIN {{ ref('stg_raw__product') }} AS product
+USING(products_id)
+
+)
+
+SELECT *
+, ROUND((revenue - purchase_cost),2) AS margin
+FROM sub_join_products_sales
+
