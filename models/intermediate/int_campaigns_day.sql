236927447d8b183e88aea0aa5147a368daa4989e
Clément G. <gandouinclement@gmail.com>
2025-07-02 10:52:10 +0000

    Correction int_campaigns_day

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,8 +1,5 @@
 SELECT 
 date_date
-, MIN(paid_source) AS paid_source
-, MIN(campaign_key) AS campaign_key
-, MIN(campaign_name) AS campaign_name
 , SUM(ads_cost) AS ads_cost
 , SUM(impression) AS impression
 , SUM(click) AS click
